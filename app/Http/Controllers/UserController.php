910d1c57388661a18ce5382c16ff682cb250f664
cristofermateo <christocheter@gmail.com>
2024-02-19 18:23:46 -0600

    users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,19 +8,27 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function test(){
-        /* Traerme todos los usuarios 
+        $user = User::create([
+            'name'=>'Horacioz',
+            'email'=>'horacio@horacioz.com',
+            'password'=>bcrypt('123123')
+
+        ]);
+
+        return $user;
+        /* Traerme todos los usuarios
         $users = User::all();
 
-        // Crear un usuario 
+        // Crear un usuario
         $user = User::create([
             'name'=>'Horacio',
             'email'=>'horacio@horacio.com',
             'password'=>bcrypt('123123')
         ]);
 
-        // Actualizar un usuario 
+        // Actualizar un usuario
 
-        // Paso 1 -> busco el usuario 
+        // Paso 1 -> busco el usuario
         $user = User::find(1);
         $user->name = 'nuevo nombre';
         $user->save();
@@ -29,7 +37,7 @@ class UserController extends Controller
         $user = User::find(1);
         $user->delete();
 
-        // Mostrar 
+        // Mostrar
         $user = User::find(1);
         */
     }
